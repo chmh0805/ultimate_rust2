71b32990f9c025ba92f9cea6ad331f1964bd55ad
chmh0805 <chmh0805@gmail.com>
2026-03-07 19:53:24 +0900

    [Feat][tutorial] sample code for rusty_engine

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,3 +1,28 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    // setup game here
+
+    game.run(GameState::default());
 }
