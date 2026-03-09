b674a193b66488cb2ccd47f4724086e3f0e91caf
chmh0805 <chmh0805@gmail.com>
2026-03-09 23:52:31 +0900

    [Feat][tutorial] mouse input

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -5,19 +5,19 @@ use rusty_engine::prelude::{
 
 #[derive(Resource)]
 struct GameState {
-    high_score: u32,
+    // high_score: u32,
     current_score: u32,
-    enemy_labels: Vec<String>,
-    spawn_timer: Timer,
+    enemy_index: i32,
+    // spawn_timer: Timer,
 }
 
 impl Default for GameState {
     fn default() -> Self {
         Self {
-            high_score: 0,
+            // high_score: 0,
             current_score: 0,
-            enemy_labels: Vec::new(),
-            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+            enemy_index: 0,
+            // spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
         }
     }
 }
@@ -70,6 +70,17 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     {
         player.translation.x += MOVEMENT_SPPED * engine.delta_f32;
     }
+
+    // handle mouse input
+    if engine.mouse_state.just_pressed(MouseButton::Left) {
+        if let Some(mouse_location) = engine.mouse_state.location() {
+            let label = format!("car{}", game_state.enemy_index);
+            game_state.enemy_index += 1;
+            let car = engine.add_sprite(label, SpritePreset::RacingCarYellow);
+            car.translation = mouse_location;
+            car.collision = true;
+        }
+    }
 }
 
 fn main() {
@@ -81,10 +92,6 @@ fn main() {
     player.rotation = SOUTH_WEST;
     player.scale = 1.0;
     player.collision = true;
-
-    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
-    car1.translation = Vec2::new(300.0, 0.0);
-    car1.collision = true;
 
     game.add_logic(game_logic);
     game.run(GameState::default());
